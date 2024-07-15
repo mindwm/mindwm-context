7f16e35525a1da4c8c9defafa0a32df59f7599f6
Your Name <you@example.com>
2024-07-15 12:05:10 +0000

    Add functionality for processing type requests "# {{ MINDWM_PREFIX }} {{ key }} = {{ value }}" and creating parameters for TmuxPane

--- func.py
+++ func.py
@@ -1,50 +1,66 @@
 from parliament import Context
+
+from neomodel import config
+from neo4j import GraphDatabase
+
+from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
+	UniqueIdProperty, RelationshipTo, RelationshipFrom, Relationship, One, OneOrMore,
+    DateTimeProperty)
+from neomodel.properties import JSONProperty
+from neomodel import db
+
+from cloudevents.http import from_http
+import sys
+import os
+import re
+
 from flask import Request
-import json
+
+neo4j_url = os.getenv("NEO4J_URL")
+neo4j_username = os.getenv("NEO4J_USERNAME")
+neo4j_password = os.getenv("NEO4J_PASSWORD")
 
 
-# parse request body, json data or URL query parameters
-def payload_print(req: Request) -> str:
-    if req.method == "POST":
-        if req.is_json:
-            return json.dumps(req.json) + "\n"
-        else:
-            # MultiDict needs some iteration
-            ret = "{"
+config.DATABASE_URL = f"bolt://{neo4j_username}:{neo4j_password}@{neo4j_url}"
+config.DRIVER = GraphDatabase.driver(f"bolt://{neo4j_url}")
 
-            for key in req.form.keys():
-                ret += '"' + key + '": "'+ req.form[key] + '", '
+class MindwmUser(StructuredNode):
+    username = StringProperty(required = True)
+    host = RelationshipTo('MindwmHost', 'HAS_MINDWM_HOST')
 
-            return ret[:-2] + "}\n" if len(ret) > 2 else "{}"
+class MindwmHost(StructuredNode):
+    hostname = StringProperty(required = True)
+    tmux = RelationshipTo('Tmux', 'HAS_TMUX', cardinality=OneOrMore)
 
-    elif req.method == "GET":
-        # MultiDict needs some iteration
-        ret = "{"
+class Tmux(StructuredNode):
+    socket_path = StringProperty(required = True)
+    host_id = IntegerProperty(required = True)
+    host = RelationshipFrom('MindwmHost', 'HAS_TMUX', cardinality=One)
+    session = RelationshipTo('TmuxSession', 'HAS_TMUX_SESSION', cardinality=OneOrMore)
 
-        for key in req.args.keys():
-            ret += '"' + key + '": "' + req.args[key] + '", '
+class TmuxSession(StructuredNode):
+    name = StringProperty(required = True)
+    tmux_id = IntegerProperty(required = True)
+    socket_path = RelationshipFrom('Tmux', 'HAS_TMUX', cardinality=One)
+    pane = RelationshipTo('TmuxPane', 'HAS_TMUX_PANE', cardinality=OneOrMore)
 
-        return ret[:-2] + "}\n" if len(ret) > 2 else "{}"
+class TmuxPane(StructuredNode):
+    pane_id = IntegerProperty(required = True)
+    session_id = IntegerProperty(required = True)
+    session = RelationshipFrom('TmuxSession', 'HAS_TMUX_PANE', cardinality=One)
+    title = StringProperty()
+    contextParameters = JSONProperty(default={})
+    io_document = Relationship('IoDocument', 'HAS_IO_DOCUMENT')
 
+class IoDocument(StructuredNode):
+    uuid = StringProperty(unique_index=True, required = True)
+    user_input = StringProperty(required = True)
+    output = StringProperty(required = True)
+    ps1 = StringProperty(required = True)
+    time = DateTimeProperty(default_now = True)
+    tmux_pane = Relationship('TmuxPane', 'HAS_IO_DOCUMENT')
 
-# pretty print the request to stdout instantaneously
-def pretty_print(req: Request) -> str:
-    ret = str(req.method) + ' ' + str(req.url) + ' ' + str(req.host) + '\n'
-    for (header, values) in req.headers:
-        ret += "  " + str(header) + ": " + values + '\n'
-
-    if req.method == "POST":
-        ret += "Request body:\n"
-        ret += "  " + payload_print(req) + '\n'
-
-    elif req.method == "GET":
-        ret += "URL Query String:\n"
-        ret += "  " + payload_print(req) + '\n'
-
-    return ret
-
- 
-def main(context: Context):
+def  main(context: Context):
     """ 
     Function template
     The context parameter contains the Flask request object and any
@@ -52,12 +68,35 @@ def main(context: Context):
     """
 
     # Add your business logic here
-    print("Received request")
+    event = from_http(context.request.headers, context.request.data)
+    data = event.data
+    payload = data['payload']
+    id = payload['id']
+    after = payload['after']
+    properties = after['properties']
+    user_input = properties['user_input']
 
-    if 'request' in context.keys():
-        ret = pretty_print(context.request)
-        print(ret, flush=True)
-        return payload_print(context.request), 200
-    else:
-        print("Empty request", flush=True)
-        return "{}", 200
+    mindwm_prefix = os.getenv("MINDWM_PREFIX", "")
+
+    pattern = fr"#\s*{re.escape(mindwm_prefix)}\s+(?P<key>\w+)\s*=\s*(?P<value>\w*)"
+    match = re.match(pattern, user_input)
+    if match:
+        key = match.group('key')
+        value = match.group('value')
+        print(f'{key} - {value}', file=sys.stderr)
+
+        results, meta = db.cypher_query(f"MATCH (n:IoDocument) WHERE ID(n) = {id}  RETURN n;")
+        iodoc = IoDocument.inflate(results[meta.index('n')][0])
+        tmux_pane = iodoc.tmux_pane.get()
+
+        if value != "":
+            print('save', file=sys.stderr)
+            tmux_pane.contextParameters[key] = value
+            tmux_pane.save()
+        else:
+            print('delete', file=sys.stderr)
+            del tmux_pane.contextParameters[key]
+            tmux_pane.save()
+
+
+    return "", 200
